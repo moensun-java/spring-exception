52da6f731cc60f427d008130c587f0498bd5018d
fengxiaotx <fengxiaotx@163.com>
2018-07-28 21:44:05 +0800

    [Bane.Shi]

---
+++ src/main/java/java/com/moensun/spring/resource/MSResourceBundle.java
@@ -0,0 +1,62 @@
+package java.com.moensun.spring.resource;
+
+import org.springframework.context.MessageSource;
+
+import java.text.MessageFormat;
+import java.util.*;
+
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 2016/11/25
+ * Time: 21:57
+ */
+public class MSResourceBundle {
+	private Locale local = Locale.getDefault();
+	private ResourceBundle resourceBundle;
+	private ResourceBundle[] resourceBundles;
+
+	public void setMessageSource(MessageSource messageScore) {
+		resourceBundle = new MessageSourceResourceBundle(messageScore,this.local);
+	}
+
+	public void setResourceBundle(String resourcePath) {
+		if(Objects.nonNull(resourcePath)){
+			String resourcePathFormat = resourcePath.replace("classpath:","");
+			resourceBundle = ResourceBundle.getBundle(resourcePathFormat, this.local);
+			setPropertiesMap(resourceBundle);
+		}
+	}
+
+	public void setResourceBundles(String[] resourcePaths) {
+		if( Objects.nonNull(resourcePaths) && resourcePaths.length>0 ){
+			List<ResourceBundle> resourceBundleList = new ArrayList<>();
+			for (int i=0;i<resourcePaths.length;i++){
+				String resourcePathFormat = resourcePaths[i].replace("classpath:","");
+				ResourceBundle resourceBundleItem = ResourceBundle.getBundle(resourcePathFormat, this.local);
+				setPropertiesMap(resourceBundleItem);
+				resourceBundleList.add(resourceBundleItem);
+			}
+			resourceBundles = resourceBundleList.toArray(new ResourceBundle[resourceBundleList.size()]);
+		}
+	}
+
+	public void setLocal(Locale local){
+		this.local = local;
+	}
+
+
+	public String text(String code,Object... params) {
+		return MessageFormat.format(ResourceProperties.codeMap.get(code),params);
+	}
+
+	public void setPropertiesMap(ResourceBundle resourceBundle){
+		Enumeration<String> keys = resourceBundle.getKeys();
+		while (keys.hasMoreElements()){
+			String key = keys.nextElement();
+			ResourceProperties.codeMap.put(key,resourceBundle.getString(key));
+		}
+	}
+
+}
---
+++ src/main/java/java/com/moensun/spring/resource/ResourceProperties.java
@@ -0,0 +1,21 @@
+package java.com.moensun.spring.resource;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 2018/7/24
+ * Time: 下午2:45
+ */
+public class ResourceProperties {
+
+    private ResourceProperties() {
+    }
+
+    public static final Map<String,String>  codeMap = new HashMap<>();
+
+}
