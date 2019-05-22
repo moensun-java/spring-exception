eea82837ed11a2cf9700f9b5bec976011cd0a5fc
Bane.Shi <bane.shi@tradeshift.com>
2019-05-22 17:02:00 +0800

    add validation exception

---
+++ src/main/java/com/moensun/spring/exception/MSValidationException.java
@@ -0,0 +1,19 @@
+package com.moensun.spring.exception;
+
+public class MSValidationException extends MSException{
+    public MSValidationException(String code, Object... params) {
+        super(code, params);
+    }
+
+    public MSValidationException(Integer httpCode, String code, Object... params) {
+        super(httpCode, code, params);
+    }
+
+    public MSValidationException(Throwable cause, String code, Object... params) {
+        super(cause, code, params);
+    }
+
+    public MSValidationException(Integer httpCode, Throwable cause, String code, Object... params) {
+        super(httpCode, cause, code, params);
+    }
+}
